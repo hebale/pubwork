961bdf49c0b1bd0cba5ad0db8c06bfe99ab2e185
hebale <hebaleman@gmail.com>
2021-05-27 00:03:05 +0900

    add polliyfill closest method

--- work/w02/js/script.js
+++ work/w02/js/script.js
@@ -73,3 +73,24 @@ var breakPoint      =       1280; // mobile < 1280 <= pc
     _this._layout   =   _layout;
     _this.ui        =   _ui;
 })(this)
+
+
+
+
+// pollyfill
+// IE9 이상 closest method 실행
+if( !Element.prototype.matches ){
+    Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+};
+if( !Element.prototype.closest ){
+    Element.prototype.closest = function(s){
+        var el = this;
+
+        do{
+            if( el.matches(s) ) return el;
+            el = el.parentElement || el.parentNode;
+        }while( el !== null || el.nodeType === 1 ){
+            return null;
+        };
+    };
+};
